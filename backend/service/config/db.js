eecc87360a003dd4d9af11320ecfc658ef73e7d1
fbismac <104845502+fbismac@users.noreply.github.com>
2022-06-13 13:09:04 -0500

    Delete backend directory

--- backend/service/config/db.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-tabs */
-/* eslint-disable indent */
-module.exports = {
-  url: "mongodb+srv://test:0Ljp0Prqa1WKHOqm@cluster0.nethn.mongodb.net/test",
-};
--- backend/service/config/log4j-config-module.js
+++
@@ -1,5 +0,0 @@
-exports.config = function() {
-  var envJSON = require('./log4j-config.json');
-  var node_env = process.env.NODE_ENV || 'development';
-  return envJSON[node_env];
-}
